4a85ff4ad9604625192200495f16fd59190c487b
Mahmut Can Gönül <75094927+MahmutCanGonul@users.noreply.github.com>
2022-02-02 23:35:31 +0300

    Update TestBitloop.py

--- TestBitloop.py
+++ TestBitloop.py
@@ -19,27 +19,40 @@ def total_cipher():
     req = requests.get('http://127.0.0.1:5000/encrypted_data_length');
     return req.json()['total_len']
 
+def total_decrypted():
+    req = requests.get('http://127.0.0.1:5000/decrypted_data_length');
+    return req.json()['total_len']
+
 def last_cipher_time():
     req = requests.get('http://127.0.0.1:5000/last_cipher_time');
     return req.json()['date']
 
+def get_keys():
+    req = requests.get('http://127.0.0.1:5000/get_keys');
+    return req.json()['keys']
+
+def encrypted_probability():
+    req = requests.get('http://127.0.0.1:5000/encrypted_probability');
+    return req.json()['probability']
     
 
 while True:
     print("'1'=>Encrypted Message")
     print("'2'=>Decrypted Message")
-    print("'3'=>Total Cipher Number")
+    print("'3'=>Total Encrypted Number")
     print("'4'=>Last Cipher Time")
     print("'5=>Remove Key'")
-    print("'6'=>exit")
+    print("'6'=>Get Keys")
+    print("'7'=>Get Encrypted Probability")
+    print("'8'=>Total Decrypted Number")
+    print("'9'=>exit")
     
     choice = input("Enter your choice? ")
     if choice == "1":
         message = input("Enter your  message?")
         if message:
             data = encrypted(message)
-            for i in range(len(data['ciphers'])):
-                print(f"{i+1}==> {data['ciphers'][i]}")
+            print(f"Encrypted data ==> {data['ciphers']}")
            
     if choice == "2":
         message = input("Enter encrypted  message?")
@@ -58,6 +71,21 @@ while True:
         if key:
             result = remove_key(key)
             print(result)
-    
     if choice == "6":
+        keys = get_keys()
+        for i in range(len(keys)):
+            insideKeys = keys[i].split(',')
+            print(f"{i+1}.Key: {insideKeys[0]} && Date: {insideKeys[1]}")
+    if choice == "7":
+        prob = encrypted_probability()
+        if prob:
+            print(prob)
+    
+    if choice == "8":
+        total = total_decrypted()
+        print(f"Total Decrypted Value: {total}")
+    
+    if choice == "9":
         break
+    
+    
